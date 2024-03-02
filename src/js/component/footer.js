feaf6afb68979dc04e7f2262421f78555cc6c5a2
aestradap <55397157+aestradap@users.noreply.github.com>
2024-03-02 17:45:34 +0000

    working on carousel

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 
-	<div className="footer fixed-bottom mt-auto py-3 text-center text-body-secondary">
+	<div className="footer 
+		fixed-bottom mt-auto
+		py-3 text-center 
+		text-body-secondary text-light">
 
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,8 +6,38 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 	
 	return <>
-		<div className="container mt-5">
-			<h2>center</h2>
+	<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div className="container d-flex justify-content-between mt-5">
+			<div className="card" style={{width: "18rem"}}>
+				<img src="..." style={{background: "grey", height: 180, width: 286}} className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+      		<div className="card" style={{width: "18rem"}}>
+				<img src="..." style={{background: "grey", height: 180, width: 286}} className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+      <div className="card" style={{width: "18rem"}}>
+				<img src="..." style={{background: "grey", height: 180, width: 286}} className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+    </div>
+    <div class="carousel-item">
+    <div className="container mt-5">
 			<div className="card" style={{width: "18rem"}}>
 				<img src="..." style={{background: "grey", height: 180, width: 286}} className="card-img-top" alt="..."/>
 				<div className="card-body">
@@ -17,5 +47,29 @@ export const Home = () => {
 				</div>
 			</div>
 		</div>
+    </div>
+    <div class="carousel-item">
+    <div className="container mt-5">
+			<div className="card" style={{width: "18rem"}}>
+				<img src="..." style={{background: "grey", height: 180, width: 286}} className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" className="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+	
 	</>
 };
